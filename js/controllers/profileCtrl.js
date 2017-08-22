1127f980f0a2647b7a150b012ac08a92c2ad6a6b
Markus Jones-Quartey <markus.jq@gmail.com>
2017-08-22 10:40:39 +0200

    Corrected rating system

--- js/controllers/profileCtrl.js
+++ js/controllers/profileCtrl.js
@@ -1,6 +1,6 @@
 angular
     .module('mainApp')
-    .controller('ProfileCtrl', ['$scope', '$http','$routeParams', ProfileCtrl]);
+    .controller('ProfileCtrl', ['$scope', '$http','$routeParams',  ProfileCtrl]);
 
 // FETCHES AD CARD DATA + stores each card's custom id as $routeParams.id
 function ProfileCtrl($scope, $http, $routeParams) {
@@ -9,4 +9,11 @@ function ProfileCtrl($scope, $http, $routeParams) {
     		return ele.id == $routeParams.id
  		 });
     });
+
+// Moves user back to index page when triggered
+    $scope.goBack = function() {
+  		window.history.back();
+	};
 }
+
+
