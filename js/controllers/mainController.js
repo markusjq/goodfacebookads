d2752a7b14af10bfcaaafc911d16fff817b846a6
Markus Jones-Quartey <markus.jq@gmail.com>
2017-08-22 08:52:57 +0200

    Improved fb ad embed load speed

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -3,25 +3,22 @@
 app.controller('mainController', ['$scope', '$http', '$sce', '$routeParams', '$route', function($scope, $http){ 
 
 	 // FETCHES AD CARD DATA
-	    $http.get('storage/ads.json').success(function(data) {
+    $http.get('storage/ads.json').success(function(data) {
         $scope.cards = data;
-    	});
+    });
 
-	$scope.getNumber = function(num) { // creates array to loop card ratings from 
-    return new Array(num);   
+    // creates array to loop card ratings from 
+	$scope.getNumber = function(num) { 
+    	return new Array(num);   
 	}
 
-
-$scope.$on('$routeChangeSuccess', function() {
-    setTimeout(FB.XFBML.parse, 500) // <-- alert from the question
-});
+	// reloads facebook iframe to display ads
+	$scope.$on('$routeChangeSuccess', function() {
+    	setTimeout(FB.XFBML.parse, 10) 
+	});
 
 
 	var searchCards = '';  // stores filter input
-
-	function reloadAd(){
-		FB.XFBML.parse();
-	}
 
 }]);
 
